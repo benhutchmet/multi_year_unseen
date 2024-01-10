c4d9cc27950b573452fe37c3486c608e78c9b0bb
benhutchmet <b.hutchins@pgr.reading.ac.uk>
2024-01-10 10:48:37 +0000

    dictoinaries and notebook added

---
+++ dictionaries.py
@@ -0,0 +1,17 @@
+# Setting up the dictoinaries for multi-year UNSEEN testing
+
+# define the uk grid from Clarke et al. 2017
+uk_grid = {
+    'lon1': -10,
+    'lon2': 3,
+    'lat1': 50,
+    'lat2': 60
+}
+
+# define the north sea grid from kay et al. 2023
+north_sea_kay = {
+    'lon1': 1, # degrees east
+    'lon2': 7,
+    'lat1': 53, # degrees north
+    'lat2': 59
+}
